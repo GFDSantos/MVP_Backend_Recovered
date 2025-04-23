305055a11f5bb6f652df638f2c62ac5d39e32ee3
Gilberto Santos <gfernandesdc@gmail.com>
2025-04-23 14:50:01 -0300

    Delete model directory

--- model/base.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import declarative_base # type: ignore
-# from sqlalchemy.ext.declarative import declarative_base
-
-# cria uma classe Base para o instanciamento de novos objetos/tabelas
-Base = declarative_base()
